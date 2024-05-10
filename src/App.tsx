abdb1d352f761dfa52b7a8277953290d404bf198
DannyS123 <szdani2012@gmail.com>
2024-05-10 17:29:05 +0200

    working chips

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,79 @@
 import { useState } from "react";
 
 import "./App.css";
-import { Chip, Stack } from "@mui/material";
+import { Chip, Grid, Stack } from "@mui/material";
 import ItemCard from "./utils/ItemCard";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const handleClick = () => {
-    console.info("You clicked the Chip.", count, setCount);
+  const [activeChip, setActiveChip] = useState(0);
+  const handleChipClick = (num: number) => {
+    setActiveChip(num);
   };
+
+  const mockBurgers = [
+    "Hamburger",
+    "Cheeseburger",
+    "Vegan Burger",
+    "Spicy Burger",
+    "Hamburger",
+    "Cheeseburger",
+    "Vegan Burger",
+    "Spicy Burger",
+    "Hamburger",
+    "Cheeseburger",
+    "Vegan Burger",
+    "Spicy Burger",
+    "Hamburger",
+    "Cheeseburger",
+    "Vegan Burger",
+    "Spicy Burger",
+  ];
+
+  const mockCoupons = [
+    "Drink Coupon",
+    "Large Meal",
+    "Family Pack",
+    "Drink Coupon",
+    "Large Meal",
+    "Family Pack",
+    "Drink Coupon",
+    "Large Meal",
+    "Family Pack",
+  ];
+
+  const mockFavorites = ["Spicy Burger", "Family Pack"];
+
+  const getItems = () => {
+    switch (activeChip) {
+      case 0:
+        return mockBurgers;
+      case 1:
+        return mockCoupons;
+      case 2:
+        return mockFavorites;
+    }
+  };
+
   return (
     <>
       <Stack
         sx={{ flexDirection: "row", justifyContent: "space-evenly", my: 1 }}
       >
-        <Chip label="Regular" onClick={handleClick} />
-        <Chip label="Coupons" onClick={handleClick} />
-        <Chip label="Favorites" onClick={handleClick} />
+        {["Regular", "Coupons", "Favorites"].map((item, idx) => (
+          <Chip
+            label={item}
+            variant={activeChip === idx ? "filled" : "outlined"}
+            onClick={() => handleChipClick(idx)}
+          />
+        ))}
       </Stack>
-      <Stack sx={{ flexWrap: "wrap", flexDirection: "row", gap: 1 }}>
-        <ItemCard title="Hamburger" logo={""} />
-        <ItemCard title="Hamburger" logo={""} />
-        <ItemCard title="Hamburger" logo={""} />
-        <ItemCard title="Hamburger" logo={""} />
-        <ItemCard title="Hamburger" logo={""} />
-        <ItemCard title="Hamburger" logo={""} />
-      </Stack>
+      <Grid container spacing={2}>
+        {getItems()?.map((burger) => (
+          <Grid item xs={6}>
+            <ItemCard title={burger} logo={""} />
+          </Grid>
+        ))}
+      </Grid>
     </>
   );
 }
